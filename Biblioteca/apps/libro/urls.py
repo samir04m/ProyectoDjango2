d9b375b35e6f19a02e037a5ad48eda7e91872ab8
Samir Mejia <samir04m@gmail.com>
2019-09-03 16:12:32 -0500

    Vistar basadas en funciones: Editar Autor

--- Biblioteca/apps/libro/urls.py
+++ Biblioteca/apps/libro/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import crearAutor, listarAutor
+from .views import crearAutor, listarAutor, editarAutor
 
 urlpatterns = [
     path('crear_autor/', crearAutor, name='crear_autor'),
     path('listar_autor/', listarAutor, name='listar_autor'),
+    path('editar_autor/<int:id>', editarAutor, name='editar_autor'),
+
 ]
--- Biblioteca/apps/libro/views.py
+++ Biblioteca/apps/libro/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 from .forms import AutorForm
 from .models import Autor
 
@@ -20,3 +21,20 @@ def crearAutor(request):
 def listarAutor(request):
     autores = Autor.objects.all()
     return render(request, 'libro/listar_autor.html', {'autores':autores})
+
+def editarAutor(request, id):
+    autor_form = None
+    error = None
+    try:
+        autor = Autor.objects.get(id=id)
+        if request.method == 'GET':
+            autor_form = AutorForm(instance=autor)
+        else:
+            autor_form = AutorForm(request.POST, instance=autor)
+            if autor_form.is_valid():
+                autor_form.save()
+            return redirect('index')
+    except ObjectDoesNotExist as e:
+        error = e
+
+    return render(request, 'libro/crear_autor.html', {'autor_form':autor_form, 'error':error})
