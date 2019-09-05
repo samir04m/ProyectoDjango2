4305b76e08e169df672146d77aa1b135c0785079
Samir Mejia <samir04m@gmail.com>
2019-09-05 15:11:43 -0500

    Sistemas de plantillas

--- blog/apps/blog/urls.py
+++ blog/apps/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
+    path('generales/', generales, name='generales'),
 ]
--- blog/apps/blog/views.py
+++ blog/apps/blog/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+def generales(request):
+    return render(request, 'generales.html')
