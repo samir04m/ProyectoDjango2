581c545c4498da469fcda1704e446a2f5cbbd608
Samir Mejia <samir04m@gmail.com>
2019-09-06 14:13:50 -0500

    Vista de detalles de un Post

--- blog/apps/blog/urls.py
+++ blog/apps/blog/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('', home, name='home'),
     path('generales/', generales, name='generales'),
+    path('post/<slug:slug>/', detallePost, name='detallePost'), #(importante colocar al final de las urls)
 ]
--- blog/apps/blog/views.py
+++ blog/apps/blog/views.py
@@ -9,3 +9,7 @@ def generales(request):
     posts = Post.objects.filter(estado=True,
                                 categoria=Categoria.objects.get(nombre='Tecnologia'))
     return render(request, 'generales.html', {"posts":posts})
+
+def detallePost(request, slug):
+    post = Post.objects.get(slug=slug)
+    return render(request, 'post.html', {"post":post})
